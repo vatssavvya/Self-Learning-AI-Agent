3bc784ea630ea4e0894a0e910344bfee34150a3e
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-06-18 19:23:50 -0400

    added stuff

--- self-learning-AI-agent/main.py
+++ self-learning-AI-agent/main.py
@@ -7,6 +7,7 @@ load_dotenv()
 
 #pick which llm to use (Anthropic is Claude, OpenAI is GPT)
 
-llm = ChatAnthropic(model="claude-3-5-sonnet-20241022") #temperature=0.7)
-#llm2 = ChatOpenAI(model="gpt-4o-mini", temperature=0.7) 
-
+#llm = ChatAnthropic(model="claude-3-5-sonnet-20241022") #temperature=0.7)
+llm2 = ChatOpenAI(model="gpt-4o-mini", temperature=0.7) 
+response = llm2.invoke("What is the meaning of life?")
+print(response)
