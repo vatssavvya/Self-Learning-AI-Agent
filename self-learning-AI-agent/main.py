7ab041d2832c82bdfefb8b9a15dfa80669f4b4d7
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-06-18 19:44:18 -0400

    chat prompt template

--- self-learning-AI-agent/main.py
+++ self-learning-AI-agent/main.py
@@ -16,6 +16,23 @@ class ResearchOutput(BaseModel):
 #pick which llm to use (Anthropic is Claude, OpenAI is GPT), we're using chat because of the free API access, but you can use the non-chat versions if you want to
 #llm = ChatAnthropic(model="claude-3-5-sonnet-20241022") #temperature=0.7)
 llm2 = ChatOpenAI(model="gpt-4o-mini", temperature=0.7) 
+parser = PydanticOutputParser(pydantic_object=ResearchOutput)
+
+prompt = ChatPromptTemplate.from_messages(
+  [
+    ("system", 
+     """
+     You are a research assistant that gathers information on a 
+     given topic and summarizes it in a clear and concise manner.
+     Answer the user query and use necessary tools. 
+     Wrap the output in this format and provide no other text
+     """,
+    ), 
+    ("placeholder", "{chat_history}"), 
+    ("human", "{query}"),
+    ("placeholder", "{agent_scratchpad}"),
+  ]
+).partial(format_instructions=parser.get_format_instructions())
 
 """ response = llm2.invoke("Is Fortnite still a good game?")
 print(response)
