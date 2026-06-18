e2e41fd98105992a87c51652a1df9b3c9eeb916c
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-06-18 19:38:50 -0400

    llm starts now

--- self-learning-AI-agent/main.py
+++ self-learning-AI-agent/main.py
@@ -2,14 +2,21 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
-#pick which llm to use (Anthropic is Claude, OpenAI is GPT), we're using chat because of the free API access, but you can use the non-chat versions if you want to
+class ResearchOutput(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
 
+#pick which llm to use (Anthropic is Claude, OpenAI is GPT), we're using chat because of the free API access, but you can use the non-chat versions if you want to
 #llm = ChatAnthropic(model="claude-3-5-sonnet-20241022") #temperature=0.7)
 llm2 = ChatOpenAI(model="gpt-4o-mini", temperature=0.7) 
-"""
-response = llm2.invoke("Is Fortnite still a good game?")
+
+""" response = llm2.invoke("Is Fortnite still a good game?")
 print(response)
 """
