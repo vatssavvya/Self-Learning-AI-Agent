49749e4e29438c08dbd2f7c233d1bdf811a4d04f
Savya Vats <savyavats@gmail.com>
2026-06-18 20:39:04 -0400

    fixed langchain version issues across different OS

--- self-learning-AI-agent/tools.py
+++ self-learning-AI-agent/tools.py
@@ -1,16 +1,13 @@
-from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun, GoogleSearchRun, PythonREPLTool, TerminalTool, ShellTool
+from langchain_community.tools import WikipediaQueryRun, DuckDuckGoSearchRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
-#custom function, you can create your own tool with a similar format
 def save_to_txt(data: str, filename: str = None) -> str:
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     formatted_text = f"{timestamp}: {data}\n"
-
     with open(filename, "a", encoding="utf-8") as file:
         file.write(formatted_text)
-
     return f"Data saved to {filename}"
 
 save_tool = Tool(
@@ -26,5 +23,5 @@ search_tool = Tool(
     description="useful for when you need to search the web"
 )
 
-api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max = 100) #if we want to return n number of results, change the number to n, and doc content increases the rate limit usage
-wikipedia_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
+wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
