3b46df8dfac99b2ba948db9af75b5fd46a9e4004
Savya Vats <68233028+vatssavyya@users.noreply.github.com>
2026-06-18 20:12:01 -0400

    added save to file template for any future custom tools

--- self-learning-AI-agent/tools.py
+++ self-learning-AI-agent/tools.py
@@ -3,6 +3,22 @@ from langchain_community.utilities import WikipediaAPIWrapper
 from langchain.tools import Tool
 from datetime import datetime
 
+#custom function, you can create your own tool with a similar format
+def save_to_txt(data: str, filename: str = None) -> str:
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    formatted_text = f"{timestamp}: {data}\n"
+
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(formatted_text)
+
+    return f"Data saved to {filename}"
+
+save_tool = Tool(
+    name="Save_Text_To_File",
+    func=save_to_txt,
+    description="useful for when you want to save some information to a text file, provide the data and filename as arguments"
+)
+
 search = DuckDuckGoSearchRun()
 search_tool = Tool(
     name="DuckDuckGo_Search",
